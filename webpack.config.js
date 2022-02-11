b4538d6fd9863f7cdf0679ff991839def8ba8b04
Bryan MacFarlane <bryanmacf@gmail.com>
2022-02-11 09:46:58 -0500

    babel

--- webpack.config.js
+++ webpack.config.js
@@ -2,32 +2,43 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
-  entry: './public/index.js',
-  output: {
-    path: path.resolve(__dirname, 'public', 'dist'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [{
-      test: /\.scss$/,
-      use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader'
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: true,
-              // options...
-            }
-          }
-        ]
-    }]
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'css/styles.css'
-    }),
-  ]
+    entry: './public/index.js',
+    output: {
+        path: path.resolve(__dirname, 'public', 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: "babel-loader",
+                  options: {
+                    presets: ['@babel/preset-env']
+                  }
+                }
+              },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true,
+                            // options...
+                        }
+                    }
+                ]
+            }]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'css/styles.css'
+        }),
+    ]
 };
